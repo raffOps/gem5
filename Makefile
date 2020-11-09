b409e98d8a657152bc2d63acaff861cc23c3a431
Rafa <rrafaeljuniorr@gmail.com>
2020-11-09 20:19:19 -0300

    txt to csv

--- Makefile
+++ Makefile
@@ -1,21 +1,21 @@
 matrix_multiplication:
-	./gem5  orgb_configs/simulate.py run-benchmark -c orgb_progs/matrix_multiplication
-	mv m5out/stats.txt m5out/stats_matrix_multiplication_l1_assoc_8.txt
-
 	./gem5  orgb_configs/simulate.py run-benchmark -c orgb_progs/matrix_multiplication --l1_assoc 1
-	mv m5out/stats.txt m5out/stats_matrix_multiplication_l1_assoc_1.txt
+	mv m5out/stats.txt m5out/matrix_multiplication_l1_assoc_1.txt
 
 	./gem5  orgb_configs/simulate.py run-benchmark -c orgb_progs/matrix_multiplication --l1_assoc 2
-	mv m5out/stats.txt m5out/stats_matrix_multiplication_l1_assoc_2.txt
+	mv m5out/stats.txt m5out/matrix_multiplication_l1_assoc_2.txt
 
 	./gem5  orgb_configs/simulate.py run-benchmark -c orgb_progs/matrix_multiplication --l1_assoc 4
-	mv m5out/stats.txt m5out/stats_matrix_multiplication_l1_assoc_4.txt
+	mv m5out/stats.txt m5out/matrix_multiplication_l1_assoc_4.txt
+
+    ./gem5  orgb_configs/simulate.py run-benchmark -c orgb_progs/matrix_multiplication
+	mv m5out/stats.txt m5out/matrix_multiplication_l1_assoc_8.txt
 
 	./gem5  orgb_configs/simulate.py run-benchmark -c orgb_progs/matrix_multiplication --l1_assoc 16
-	mv m5out/stats.txt m5out/stats_matrix_multiplication_l1_assoc_16.txt
+	mv m5out/stats.txt m5out/matrix_multiplication_l1_assoc_16.txt
 
 	./gem5  orgb_configs/simulate.py run-benchmark -c orgb_progs/matrix_multiplication --l1_assoc 32
-	mv m5out/stats.txt m5out/stats_matrix_multiplication_l1_assoc_32.txt
+	mv m5out/stats.txt m5out/matrix_multiplication_l1_assoc_32.txt
 
-	python m5out/parse.py
+	python m5out/parse.py matrix_multiplication
 
--- m5out/parse.py
+++ m5out/parse.py
@@ -1,18 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[251]:
+
+
 import re
 import os
 import json
+import pandas as pd
+import numpy as np
+import sys
 
 
-for file_name in os.listdir(os.getcwd()):
-    if file_name.startswith("stats") and file_name.endswith(".txt"):
-        with open(file_name) as file:
-            linhas = file.readlines()
-            stats = dict([re.findall("([^ ]*) *([0-9]*[.]?[0-9]*).*#",linha)[0] for linha in linhas[2:-2]])
-        with open(file_name.replace("txt","json"), "w") as file:
-            json.dump(stats, file)
+# In[249]:
 
 
+def txt_to_dataframe(algoritmo):
+    csv = []
+    for file_name in sorted(os.listdir()):
+        if file_name.startswith(algoritmo) and file_name.endswith(".txt"):
+            with open(file_name) as file:
+                linhas = file.readlines()
+                metricas = dict([re.findall("([^ ]*) *([0-9]*[.]?[0-9]*).*#",linha)[0] for linha in linhas[2:-2]])
+                parametro = re.findall("(.*)_(.*)_(.*).txt", file_name)[0]
+                metricas[parametro[1]] = parametro[2]
+                csv.append(metricas)
+    df = pd.DataFrame(csv)
+    df[parametro[1]]= df[parametro[1]].astype(np.uint8)
+    df.sort_values(parametro[1], ascending=True, inplace=True)
+    return df[[ parametro[1],
+                "sim_seconds", 
+                "system.cpu.ipc", 
+                "system.cpu.dcache.overall_miss_rate::total", 
+                "system.l2cache.overall_miss_rate::total", 
+                "system.l3cache.overall_miss_rate::total" ]].sort_values(parametro[1])
 
+
+# In[250]:
+
+
+if __name__ == "__main__":
+    df = txt_to_dataframe(sys.argv[1])
+    df.to_csv(sys.argv[1]+".csv")
+
+
+# In[ ]:
 
 
 

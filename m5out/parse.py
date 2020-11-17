858acd5d11c86ceaf6d7bbfbd1db9a525bf2d3d8
Rafa <rrafaeljuniorr@gmail.com>
2020-11-16 22:14:45 -0300

    finalizado

--- m5out/parse.py
+++ m5out/parse.py
@@ -15,25 +15,41 @@ import sys
 # In[ ]:
 
 
-def txt_to_dataframe(algoritmo):
+def txt_to_dataframe(algoritmo, parametro):
     csv = []
     for file_name in sorted(os.listdir()):
-        if file_name.startswith(algoritmo) and file_name.endswith(".txt"):
+        if file_name.startswith(algoritmo+"_"+parametro) and file_name.endswith(".txt"):
             with open(file_name) as file:
                 linhas = file.readlines()
                 metricas = dict([re.findall("([^ ]*) *([0-9]*[.]?[0-9]*).*#",linha)[0] for linha in linhas[2:-2]])
-                parametro = re.findall("(.*)_(.*)_(.*).txt", file_name)[0]
-                #print(parametro)
-                metricas[parametro[1]] = int(parametro[2])
+                variavel = file_name[len(algoritmo+"_"+parametro+"_"):-4]
+                try:
+                    metricas[parametro] = int(variavel)
+                except:
+                    metricas[parametro] = variavel
                 csv.append(metricas)
     df = pd.DataFrame(csv)
-    df.sort_values(parametro[1], ascending=True, inplace=True)
-    return df[[ parametro[1],
-                "sim_seconds", 
-                "system.cpu.ipc", 
-                "system.cpu.dcache.overall_miss_rate::total", 
-                "system.l2cache.overall_miss_rate::total", 
-                "system.l3cache.overall_miss_rate::total" ]].sort_values(parametro[1])
+    df.sort_values(parametro, ascending=True, inplace=True)
+    if algoritmo == "MatrixMultiplication" and parametro == "branch predictor":
+        segundo_parametro = "system.cpu.branchPred.condIncorrect"
+    elif algoritmo == "MatrixMultiplication" and parametro == "l1 associatividade":
+        segundo_parametro = "system.cpu.dcache.overall_miss_rate::total"
+    elif algoritmo == "MatrixMultiplication" and parametro == "l1 size":
+        segundo_parametro = "sim_seconds"
+    elif algoritmo == "CustoMinimo" and parametro == "branch predictor":
+        segundo_parametro = "system.cpu.branchPred.condIncorrect"
+    elif algoritmo == "CustoMinimo" and parametro == "l1 associatividade":
+        segundo_parametro = "system.cpu.dcache.overall_miss_rate::total"
+    elif algoritmo == "CustoMinimo" and parametro == "l1 size":
+        segundo_parametro = "sim_seconds"
+    elif algoritmo == "Mergesort" and parametro == "branch predictor":
+        segundo_parametro = "system.cpu.branchPred.condIncorrect"
+    elif algoritmo == "Mergesort" and parametro == "l1 associatividade":
+        segundo_parametro = "system.cpu.dcache.overall_miss_rate::total"
+    elif algoritmo == "Mergesort" and parametro == "l1 size":
+        segundo_parametro = "sim_seconds"
+
+    return df[[ parametro, segundo_parametro]].sort_values(parametro)
 
 
 # In[ ]:
@@ -41,6 +57,7 @@ def txt_to_dataframe(algoritmo):
 
 if __name__ == "__main__":
     algoritmo = sys.argv[1]
-    df = txt_to_dataframe(algoritmo)
-    df.to_csv(sys.argv[1]+".csv")
+    parametro = sys.argv[2]
+    df = txt_to_dataframe(algoritmo, parametro)
+    df.to_csv(algoritmo+"_"+parametro+".csv")
 
